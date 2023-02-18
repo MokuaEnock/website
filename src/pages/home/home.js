09b811f06a8d07fc7ddcf7f6d689b7d469a563b0
Enock <enock.mokua@student.moringaschool.com>
2023-02-19 00:10:30 +0300

    create navigation bar and style it

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,5 +1,25 @@
 import "./home.css";
+import { FaInstagram, FaTwitter, FaLinkedin, FaFacebook } from "react-icons/fa";
 
 export default function Home() {
-  return <main>Home page</main>;
+  return (
+    <main id="home-page">
+      <nav id="navigation-bar">
+        <div id="navigation-container">
+          <a href="https://www.instagram.com/">
+            <FaInstagram className="social-icons" />
+          </a>
+          <a href="https://twitter.com/">
+            <FaTwitter className="social-icons" />
+          </a>
+          <a href="https://www.linkedin.com/">
+            <FaLinkedin className="social-icons" />
+          </a>
+          <a href="https://www.facebook.com/">
+            <FaFacebook className="social-icons" />
+          </a>
+        </div>
+      </nav>
+    </main>
+  );
 }
