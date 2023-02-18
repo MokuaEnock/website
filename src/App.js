a71a6fcdce188f2b2b142fd5a77279a4a9a98b0a
Enock <enock.mokua@student.moringaschool.com>
2023-02-18 22:03:45 +0300

    connect button to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
+import Button from "./components/button/button";
+
 function App() {
-  return <div className="App">kichapo</div>;
+  return (
+    <div className="App">
+      <Button title="take"/>
+    </div>
+  );
 }
 
 export default App;
