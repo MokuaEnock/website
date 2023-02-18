d06d42dd2e2f60e9aa392a761aa4e2e1ec018d2a
Enock <enock.mokua@student.moringaschool.com>
2023-02-18 23:51:03 +0300

    connect home to app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 
-import Header from "./components/header/header";
-import Button from "./components/button/button";
+import Home from "./pages/home/home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="" element="" />
+          <Route path="/" element={<Home />} />
           <Route path="" element="" />
           <Route path="" element="" />
           <Route path="" element="" />
