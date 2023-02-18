466935cec7f69d88eb999949da79b42acffd56fc
Enock <enock.mokua@student.moringaschool.com>
2023-02-18 23:29:56 +0300

    create home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
 
+import Header from "./components/header/header";
 import Button from "./components/button/button";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App" onClick={() => alert("button clicked")}>
-      <Button text="take" />
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="" element="" />
+          <Route path="" element="" />
+          <Route path="" element="" />
+          <Route path="" element="" />
+          <Route path="" element="" />
+          <Route path="" element="" />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
