758f5be780797675e665684ce6a8bc48ef74d5e6
Enock <enock.mokua@student.moringaschool.com>
2023-02-19 18:43:21 +0300

    create services component

--- src/App.js
+++ src/App.js
@@ -4,21 +4,22 @@ import Home from "./pages/home/home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/header/header";
 import Footer from "./components/footer/Footer";
-import Navbar from "./components/NavBar/Navbar";
+import Services from "./pages/Services/services";
+
 function App() {
   return (
-      <BrowserRouter className="app">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="" element="" />
-          <Route path="" element="" />
-          <Route path="" element="" />
-          <Route path="" element="" />
-          <Route path="" element="" />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+    <BrowserRouter className="app">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="" element="" />
+        <Route path="" element="" />
+        <Route path="" element="" />
+        <Route path="" element="" />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Services/services.js
@@ -0,0 +1,5 @@
+import "./services.css";
+
+export default function Services() {
+  return <main className="services">This is the services</main>;
+}
