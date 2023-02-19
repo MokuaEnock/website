65edce19ddc3c7a78d83ab7346d4579e545b7048
Enock <enock.mokua@student.moringaschool.com>
2023-02-19 15:45:59 +0300

    create footer container and add styling

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -11,7 +11,33 @@ export default function Footer() {
           <button>Subscribe</button>
         </form>
       </div>
-      <div className="footer-container"></div>
+      <div className="footer-container">
+        <span className="footer-logo"></span>
+        <span>
+          <h3>Services</h3>
+          <Link to="/services">Services</Link>
+          <Link to="/products">Products</Link>
+          <Link to="/charity">Charity</Link>
+        </span>
+        <span>
+          <h3>About Us</h3>
+          <Link to="/impact">Our Impact</Link>
+          <Link to="/team">Team</Link>
+          <Link to="/gallery">Gallery</Link>
+          <Link to="/partners">Partners</Link>
+        </span>
+        <span>
+          <h3>News</h3>
+          <Link to="/blog">Blog</Link>
+          <Link to="/newsroom">Newsroom</Link>
+          <Link to="/events">Events</Link>
+        </span>
+        <span>
+          <Link to="/faqs">FAQs</Link>
+          <Link to="/careers">Careers</Link>
+          <Link to="/contact">Contact Us</Link>
+        </span>
+      </div>
       <div className="footer-foot">
         <p>&copy; 2023 Kilimo.Africa All rights reserved.</p>
         <Link to="/privacy">Privacy policy</Link>
