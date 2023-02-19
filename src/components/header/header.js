582059892b92770a8923438082c3100b4fb0cbed
Enock <enock.mokua@student.moringaschool.com>
2023-02-19 12:57:20 +0300

    add select tags for varous scetions

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,16 +3,47 @@ import "./header.css";
 // import Button from "../button/button";
 
 export default function Header(props) {
+  function handleSelectChange(event) {
+    let url = event.target.value;
+    window.location.href = url;
+  }
   return (
     <header className="header">
       <span className="header-logo">
         <Link to="/">Kilimo.Africa</Link>
       </span>
       <span className="header-info">
-        <Link to="/services">Services</Link>
-        <Link to="/about">About Us</Link>
+        <select className="services-header" onChange={handleSelectChange}>
+          <option value="" disabled selected>
+            Services
+          </option>
+          <option value="/services">Services</option>
+          <option value="/products">Products</option>
+          <option value="/charity">Charity</option>
+        </select>
+
+        <select className="services-header" onChange={handleSelectChange}>
+          <option value="" disabled selected>
+            About Us
+          </option>
+          <option value="/impact">Our Impact</option>
+          <option value="/products">Team</option>
+          <option value="/gallery">Gallery</option>
+          <option value="/partners">Partners</option>
+        </select>
+
+        <select className="services-header" onChange={handleSelectChange}>
+          <option value="" disabled selected>
+            News
+          </option>
+          <option value="/blog">Blog</option>
+          <option value="/newsroom">Newsroom</option>
+          <option value="/events">Events</option>
+          <option value="/recipes">Recipes</option>
+        </select>
+
+        <Link to="/blog">Careers</Link>
         <Link to="/contact">Contact Us</Link>
-        <Link to="/blog">Blog</Link>
       </span>
       <span className="header-logs">
         <Link to="login">Log In</Link>
