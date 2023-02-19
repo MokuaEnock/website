c6e7a1a5d6d676ad5136422e47a083b920c4ecc9
Enock <enock.mokua@student.moringaschool.com>
2023-02-19 14:52:57 +0300

    create footer foot and style it

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,19 @@
+import { Link } from "react-router-dom";
 import "./Footer.css";
 
 export default function Footer() {
-  return <footer></footer>;
+  return (
+    <footer>
+      <div className="footer-head">
+        <span></span>
+        <form></form>
+      </div>
+      <div className="footer-container"></div>
+      <div className="footer-foot">
+        <p>&copy; 2023 Kilimo.Africa All rights reserved.</p>
+        <Link to="/privacy">Privacy policy</Link>
+        <Link to="/terms">Terms and conditions</Link>
+      </div>
+    </footer>
+  );
 }
