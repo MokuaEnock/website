a0d98e237ffc3b3db82c8faa5c93bb6700a6fe75
Enock <enock.mokua@student.moringaschool.com>
2023-02-19 11:30:05 +0300

    create links to various pages in header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,21 @@
+import { Link } from "react-router-dom";
 import "./header.css";
 
 export default function Header(props) {
-  return <header className="header">
-    this is the header
-  </header>;
+  return (
+    <header className="header">
+      <span className="header-logo">
+        <Link to="/">Home</Link>
+      </span>
+      <span className="header-info">
+        <Link to="/services">Services</Link>
+        <Link to="/about">About Us</Link>
+        <Link to="/contact">Contact Us</Link>
+        <Link to="/blog">Blog</Link>
+      </span>
+      <span className="header-logs">
+        <Link to="login">Log In</Link>
+      </span>
+    </header>
+  );
 }
