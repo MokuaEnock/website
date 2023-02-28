e82f05f6c2684d3cce00f84976cf2936d53e4377
Enock <enock.mokua@student.moringaschool.com>
2023-03-01 02:55:18 +0300

    add basic structure to team section

--- src/pages/Team/team.js
+++ src/pages/Team/team.js
@@ -1,3 +1,20 @@
+import "./team.css";
+
+import Button from "../../components/button/button";
 export default function Team() {
-  return <main>This is the team</main>;
+  return (
+    <main id="team">
+      <h1>Meet the Team</h1>
+      <p>
+        Kijani Africa was founded by Andreen Muchiri and Enock Mokua with a
+        shared vision to create innovative solutions that solve real-world
+        problems. Our team may be small, but we are passionate about delivering
+        exceptional products and services to our clients.
+      </p>
+
+      <section id="team-containers">
+        <div className="team-container"></div>
+      </section>
+    </main>
+  );
 }
