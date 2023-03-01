e77dbd95fb98b40435db92c3ae867f8f1fe00b39
Enock <enock.mokua@student.moringaschool.com>
2023-03-01 14:38:30 +0300

    create the waitlist component

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,5 +1,6 @@
 import "./home.css";
 import { useNavigate } from "react-router-dom";
+import Button from "../../components/button/button";
 
 export default function Home() {
   let redirect = useNavigate();
@@ -9,7 +10,19 @@ export default function Home() {
   }
 
   function WaitList() {
-    return <form id="waitlist-form">This is the waitlist</form>;
+    return (
+      <section id="waitlist-form">
+        <h2>Join the Kijani Africa Waitlist</h2>
+        <p>
+          We're currently preparing for our upcoming launch, and we'd love to
+          have you on board as a member of our waitlist. By signing up, you'll
+          be one of the first to know when we're ready to start collecting and
+          processing waste, and you'll receive exclusive access to early bird
+          pricing and other special offers.
+        </p>
+        <Button />
+      </section>
+    );
   }
   return (
     <main id="home-page">
