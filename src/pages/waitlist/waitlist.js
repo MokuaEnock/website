ead2011dbb697d3fe68c3a7986aac8ac36d0aeb2
Enock <enock.mokua@student.moringaschool.com>
2023-03-01 16:02:49 +0300

    add react odel and change button for submit wait form

--- src/pages/waitlist/waitlist.js
+++ src/pages/waitlist/waitlist.js
@@ -2,6 +2,10 @@ import Button from "../../components/button/button";
 import "./waitlist.css";
 
 export default function WaitL() {
+  function handleJoin(e) {
+    e.preventDefault();
+  }
+
   return (
     <main id="wait-page">
       <h1>
@@ -9,15 +13,23 @@ export default function WaitL() {
         Kijani Africa
       </h1>
       <form id="wait-form">
-        <input type="text" placeholder="Enter your first name" />
-        <input type="text" placeholder="Enter your last name" />
-        <input type="text" placeholder="Enter your E-Mail address" />
+        <span>
+          <input type="text" placeholder="Enter your first name" required />
+        </span>
+
+        <span>
+          <input type="text" placeholder="Enter your last name" required />
+        </span>
+
+        <span>
+          <input type="text" placeholder="Enter your E-Mail address" required />
+        </span>
         <textarea placeholder="Any additional feedback" />
-
-        <Button text="Join the waitlist" />
+        <button type="submit">
+          <p>Join the waitlist</p>
+        </button>
+        {/* <Button text="Join the waitlist" onClick={handleJoin} /> */}
       </form>
-
-      
     </main>
   );
 }
