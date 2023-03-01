ca4583e264bcb05cf46fff8516e0990c87ad8ab2
Enock <enock.mokua@student.moringaschool.com>
2023-03-01 15:30:20 +0300

    add content to signup form

--- src/pages/waitlist/waitlist.js
+++ src/pages/waitlist/waitlist.js
@@ -1,5 +1,23 @@
+import Button from "../../components/button/button";
 import "./waitlist.css";
 
 export default function WaitL() {
-  return <main>This is the waitlist</main>;
+  return (
+    <main id="wait-page">
+      <h1>
+        Join the waitlist for <br />
+        Kijani Africa
+      </h1>
+      <form id="wait-form">
+        <input type="text" placeholder="Enter your first name" />
+        <input type="text" placeholder="Enter your last name" />
+        <input type="text" placeholder="Enter your E-Mail address" />
+        <textarea placeholder="Any additional feedback" />
+
+        <Button text="Join the waitlist" />
+      </form>
+
+      
+    </main>
+  );
 }
